fbb62758f7fb969099b2c900ed31dc35c4a9a829
Akhilesh Muthusamy <40685207+AkhileshMuthusamy@users.noreply.github.com>
2019-11-12 16:06:33 +0530

    Updated validtation code

--- validation.js
+++ validation.js
@@ -1,6 +1,20 @@
 $(".onlyNumber").keypress(function (e) {
-        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+        if (e.keyCode != 8 && e.keyCode != 0 && (e.keyCode < 48 || e.keyCode > 57)) {
             return false;
+        }
+    });
+
+    $(".onlyNumber").on('paste', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        let cd = e.originalEvent.clipboardData;
+        let intValue = cd.getData("text/plain")
+        console.log(intValue);
+        if (!Number.isNaN(parseInt(intValue))) {
+            if(parseInt(intValue) >= 0) {
+                e.target.value = parseInt(intValue);
+            }
         }
     });
 
@@ -14,8 +28,7 @@ $(".onlyNumber").keypress(function (e) {
         }
     });
 
-// Validate data on paste
-$(".onlyNumberAndDecimal").on('paste', function(e) {
+    $(".onlyNumberAndDecimal").on('paste', function(e) {
         e.stopPropagation();
         e.preventDefault();
 
@@ -23,8 +36,20 @@ $(".onlyNumberAndDecimal").on('paste', function(e) {
         let decValue = cd.getData("text/plain")
         console.log(decValue);
         if (!Number.isNaN(parseFloat(decValue))) {
-            e.target.value = parseFloat(decValue);
+            if(parseFloat(decValue) >= 0) {
+                e.target.value = parseFloat(decValue);
+            }
         }
     });
+
+
+$(document).on("copy", function(e){
+  e.stopPropagation();
+  e.preventDefault();
+                
+  var cd = e.originalEvent.clipboardData;
+  cd.setData("text/plain", "nice, eh?!");
+ });
     
+// Decimal with (13,7)
     ^\s*(?=.*[1-9])\d{1,13}(?:\.\d{1,7})?\s*$
